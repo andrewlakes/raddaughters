b8a9dbbf8fbef857b5e7e8f04acb24411e2be30f
Julian Rees <julianrees@users.noreply.github.com>
2019-02-24 07:09:57 -0800

    code cleanup and commenting, instructions on structure of Isotope data

--- getisodata.R
+++ getisodata.R
@@ -3,19 +3,19 @@ library(PeriodicTable)
 library(plyr)
 data("periodicTable")
 
+
 Isotopes <- list()
 
-isofile <- 'isotopes/JEFF33-rdd_all.asc'
+isofile <- 'isotopes/JEFF33-rdd_all.asc' # this is the master isotope data file
 
-#which(regexpr('213BI DECAY', readLines(con = 'isotopes/JEFF33-rdd_all.asc')) == 1)
-
+# custom function for splitting lines at whitespaces and unlisting
 linesplit <- function(x) unlist(strsplit(x, split = " "))[
   which(unlist(strsplit(x, split = " ")) != "")]
 
+iso <- '227TH' # input the parent isotope! 
+branchThreshold <- 0.0001 # input the branch percentage threshold for abandoning a branch
 
-iso <- '225AC' # input the parent isotope! 
-branchThreshold <- 0.0001
-
+# start making the new isotope in the addIso list
 addIso <- list()
 addIso$isotope <- iso
 addIso$decayLevel <- 0
@@ -27,7 +27,7 @@ addIso$Z = periodicTable$numb[
   which(match(periodicTable$symb, addIso$symb) == 1)]
 addIso$masterYield <- 1
 
-
+# get the half-life data, check for units and convert to days 
 hfl <- linesplit(grep(iso, readLines(con = 'isotopes/JEFF33-rdd_all.asc'), value = TRUE)[1])
 
 if (hfl[4] == 'Y'){
@@ -46,8 +46,10 @@ if (hfl[4] == 'Y'){
 
 addIso$t12 <- t12
 
+# check for which lines match the decay information for the isotope of interest
 linematches <- grep(iso, readLines(con = 'isotopes/JEFF33-rdd_all.asc'), value = FALSE)
-# ndk_line <- grep(iso, readLines(con = 'isotopes/JEFF33-rdd_all.asc'), value = FALSE)[1]+1
+
+# number of decays as read from the data file
 ndk <- as.integer(linesplit(readLines(con = isofile)[linematches[1]+1])[5])
 
 Decays <- list()
@@ -56,42 +58,45 @@ Beta <- list()
 Positron <- list()
 EC <- list()
 IT <- list()
+
+# search through the data file and get the lines that match the isotope, find the decay type(s)
 for (i in seq(ndk)) {
   outp <- linesplit(readLines(con = 'isotopes/JEFF33-rdd_all.asc')[linematches[i+1]])
   outq <- linesplit(readLines(con = 'isotopes/JEFF33-rdd_all.asc')[linematches[i+1]+1])
   
   if (outp[2] == 'A') {
     Alpha$Q <- as.numeric(outp[6])
-    Alpha$BranchYield <- as.numeric(outq[2])/100
-    Alpha$BrQ <- Alpha$Q * Alpha$BranchYield
-    Alpha$Daughter <- paste(addIso$A-4, periodicTable$symb[which(periodicTable$numb == addIso$Z-2)], sep = '')
+    Alpha$branchYield <- as.numeric(outq[2])/100
+    Alpha$BrQ <- Alpha$Q * Alpha$branchYield
+    Alpha$daughter <- paste(addIso$A-4, periodicTable$symb[which(periodicTable$numb == addIso$Z-2)], sep = '')
   } 
   if (outp[2] == 'B-') {
     Beta$Q <- as.numeric(outp[6])
-    Beta$BranchYield <- as.numeric(outq[2])/100
-    Beta$BrQ <- Beta$Q * Beta$BranchYield
-    Beta$Daughter <- paste(addIso$A, periodicTable$symb[which(periodicTable$numb == addIso$Z+1)], sep = '')
+    Beta$branchYield <- as.numeric(outq[2])/100
+    Beta$BrQ <- Beta$Q * Beta$branchYield
+    Beta$daughter <- paste(addIso$A, periodicTable$symb[which(periodicTable$numb == addIso$Z+1)], sep = '')
   } 
   if (outp[2] == 'B+') {
     Positron$Q <- as.numeric(outp[6])
-    Positron$BranchYield <- as.numeric(outq[2])/100
-    Positron$BrQ <- Positron$Q * Positron$BranchYield
-    Positron$Daughter <- paste(addIso$A, periodicTable$symb[which(periodicTable$numb == addIso$Z-1)], sep = '')
+    Positron$branchYield <- as.numeric(outq[2])/100
+    Positron$BrQ <- Positron$Q * Positron$branchYield
+    Positron$daughter <- paste(addIso$A, periodicTable$symb[which(periodicTable$numb == addIso$Z-1)], sep = '')
   } 
   if (outp[2] == 'EC') {
     EC$Q <- as.numeric(outp[6])
-    EC$BranchYield <- as.numeric(outq[2])/100
-    EC$BrQ <- EC$Q * EC$BranchYield
-    EC$Daughter <- paste(addIso$A, periodicTable$symb[which(periodicTable$numb == addIso$Z-1)], sep = '')
+    EC$branchYield <- as.numeric(outq[2])/100
+    EC$BrQ <- EC$Q * EC$branchYield
+    EC$daughter <- paste(addIso$A, periodicTable$symb[which(periodicTable$numb == addIso$Z-1)], sep = '')
   } 
   if (outp[2] == 'IT') {
     IT$Q <- as.numeric(outp[6])
-    IT$BranchYield <- as.numeric(outq[2])/100
-    IT$BrQ <- EC$Q * EC$BranchYield
-    IT$Daughter <- paste(addIso$A, addiso$symb, sep = '')
+    IT$branchYield <- as.numeric(outq[2])/100
+    IT$BrQ <- EC$Q * EC$branchYield
+    IT$daughter <- paste(addIso$A, addiso$symb, sep = '')
   } 
 }
 
+# populate the list Decays, and remove the empty ones
 Decays$Alpha <- Alpha
 Decays$Beta <- Beta
 Decays$Positron <- Positron
@@ -100,16 +105,18 @@ Decays$IT <- IT
 Decays <- Decays[-which(lapply(Decays, length) == 0)]
 addIso$Decays <- Decays
 
+# add the new isotope to the Isotopes list
 Isotopes[[length(Isotopes)+1]] <- addIso
 names(Isotopes)[length(Isotopes)] <- iso
 
 
-
 #---- Starting to add daughter isotopes ----
 
-dk_levs <- 9
+dk_levs <- 9 # choose the number of levels of the decay chain to follow
 
 declevs <- array()
+
+# make an array of isotopes at a given decay level (i.e. no repeating)
 for (i in seq(dk_levs)) {
   for (n in seq(length(Isotopes))) {
     declevs[n] <- Isotopes[[n]]$decayLevel
@@ -117,9 +124,10 @@ for (i in seq(dk_levs)) {
   
   isos <- which(declevs == i-1)
   
+  # repeat loop above
   for (j in isos) {
     for (dk in seq(length(Isotopes[[j]]$Decays))) {
-      iso <- toupper(Isotopes[[j]]$Decays[[dk]]$Daughter)
+      iso <- toupper(Isotopes[[j]]$Decays[[dk]]$daughter)
       
       addIso <- list()
       addIso$isotope <- iso
@@ -130,17 +138,19 @@ for (i in seq(dk_levs)) {
         which(match(tolower(periodicTable$symb), tolower(addIso$symb)) == 1)]
       addIso$Z = periodicTable$numb[
         which(match(periodicTable$symb, addIso$symb) == 1)]
-      addIso$masterYield <- Isotopes[[j]]$Decays[[dk]]$BranchYield * Isotopes[[j]]$masterYield
+      addIso$masterYield <- Isotopes[[j]]$Decays[[dk]]$branchYield * Isotopes[[j]]$masterYield
       
+      # check to see if the branch is insignificant
       if (addIso$masterYield < branchThreshold) {
-        paste('The ', addIso$A, addIso$symb, ' branch has been terminated due to insufficient yield',
-              sep = '')
+        print(paste('The ', addIso$A, addIso$symb, ' branch has been terminated due to insufficient yield',
+              sep = ''))
         next
       }
       
+      # check to see if there is a half-life!  If not, the daughter isotope is stable by JEFF definitions
       hfl <- linesplit(grep(iso, readLines(con = 'isotopes/JEFF33-rdd_all.asc'), value = TRUE)[1])
       if (length(hfl) == 0) {
-        print('You have reached a stable isotope!')
+        stop('You have reached a stable isotope!')
       }
       
       if (hfl[4] == 'Y'){
@@ -176,33 +186,33 @@ for (i in seq(dk_levs)) {
         
         if (outp[2] == 'A') {
           Alpha$Q <- as.numeric(outp[6])
-          Alpha$BranchYield <- as.numeric(outq[2])/100
-          Alpha$BrQ <- Alpha$Q * Alpha$BranchYield
-          Alpha$Daughter <- paste(addIso$A-4, periodicTable$symb[which(periodicTable$numb == addIso$Z-2)], sep = '')
+          Alpha$branchYield <- as.numeric(outq[2])/100
+          Alpha$BrQ <- Alpha$Q * Alpha$branchYield
+          Alpha$daughter <- paste(addIso$A-4, periodicTable$symb[which(periodicTable$numb == addIso$Z-2)], sep = '')
         } 
         if (outp[2] == 'B-') {
           Beta$Q <- as.numeric(outp[6])
-          Beta$BranchYield <- as.numeric(outq[2])/100
-          Beta$BrQ <- Beta$Q * Beta$BranchYield
-          Beta$Daughter <- paste(addIso$A, periodicTable$symb[which(periodicTable$numb == addIso$Z+1)], sep = '')
+          Beta$branchYield <- as.numeric(outq[2])/100
+          Beta$BrQ <- Beta$Q * Beta$branchYield
+          Beta$daughter <- paste(addIso$A, periodicTable$symb[which(periodicTable$numb == addIso$Z+1)], sep = '')
         } 
         if (outp[2] == 'B+') {
           Positron$Q <- as.numeric(outp[6])
-          Positron$BranchYield <- as.numeric(outq[2])/100
-          Positron$BrQ <- Positron$Q * Positron$BranchYield
-          Positron$Daughter <- paste(addIso$A, periodicTable$symb[which(periodicTable$numb == addIso$Z-1)], sep = '')
+          Positron$branchYield <- as.numeric(outq[2])/100
+          Positron$BrQ <- Positron$Q * Positron$branchYield
+          Positron$daughter <- paste(addIso$A, periodicTable$symb[which(periodicTable$numb == addIso$Z-1)], sep = '')
         } 
         if (outp[2] == 'EC') {
           EC$Q <- as.numeric(outp[6])
-          EC$BranchYield <- as.numeric(outq[2])/100
-          EC$BrQ <- EC$Q * EC$BranchYield
-          EC$Daughter <- paste(addIso$A, periodicTable$symb[which(periodicTable$numb == addIso$Z-1)], sep = '')
+          EC$branchYield <- as.numeric(outq[2])/100
+          EC$BrQ <- EC$Q * EC$branchYield
+          EC$daughter <- paste(addIso$A, periodicTable$symb[which(periodicTable$numb == addIso$Z-1)], sep = '')
         } 
         if (outp[2] == 'IT') {
           IT$Q <- as.numeric(outp[6])
-          IT$BranchYield <- as.numeric(outq[2])/100
-          IT$BrQ <- EC$Q * EC$BranchYield
-          IT$Daughter <- paste(addIso$A, addiso$symb, sep = '')
+          IT$branchYield <- as.numeric(outq[2])/100
+          IT$BrQ <- EC$Q * EC$branchYield
+          IT$daughter <- paste(addIso$A, addiso$symb, sep = '')
         } 
       }
       
@@ -214,6 +224,7 @@ for (i in seq(dk_levs)) {
       Decays <- Decays[-which(lapply(Decays, length) == 0)]
       addIso$Decays <- Decays
       
+      # check to see if two branches are converging, and if so add the masterYield parameters
       if (length(Isotopes) > 1) {
         if (any(match(names(Isotopes),addIso$isotope), na.rm = TRUE)) {
           Isotopes[[which(match(names(Isotopes),addIso$isotope) == 1)]]$masterYield <- 
@@ -231,4 +242,4 @@ for (i in seq(dk_levs)) {
 }
 
 
-Isotopes$`213BI`$Decays$Beta$Daughter
+Isotopes$`213BI`$Decays$Beta$daughter
